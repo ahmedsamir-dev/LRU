862b369bb80bc6513203900b7ac16c3a713ed7e7
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2020-12-24 18:50:25 +0200

    Add Statistics

--- LRU.cpp
+++ LRU.cpp
@@ -6,6 +6,23 @@
 
 using namespace std;
 
+class Statistics
+{
+private:
+  int keys, hits, misses;
+
+public:
+  Statistics()
+  {
+    keys = 0;
+    hits = 0;
+    misses = 0;
+  }
+
+  template <typename K, typename V>
+  friend class LruCache;
+};
+
 template <typename K, typename V>
 class LruCache
 {
@@ -13,23 +30,20 @@ private:
   int capacity;
   list<K> storage;
   unordered_map<K, pair<V, typename list<K>::iterator>> searcher;
+  Statistics stats;
 
-  bool isHere(K key)
+  struct StatisticsDummy
   {
-    return searcher.find(key) != searcher.end();
-  }
+    int keys, hits, misses;
+    StatisticsDummy(int keys, int hits, int misses) : keys{keys}, hits{hits}, misses{misses}
+    {
+    }
+  };
 
   pair<typename list<K>::iterator, V> getPositionAndValue(const K key)
   {
     auto item = searcher.find(key);
-
     return {item->second.second, item->second.first};
-  }
-
-  void deleteLeastVisited()
-  {
-    searcher.erase(storage.back());
-    storage.pop_back();
   }
 
   /* data */
@@ -37,6 +51,18 @@ public:
   LruCache(int capacity) : capacity(capacity)
   {
     this->capacity = capacity;
+  }
+
+  bool isHere(K key)
+  {
+    if (searcher.find(key) != searcher.end())
+    {
+      stats.hits++;
+      return true;
+    }
+    stats.misses++;
+
+    return false;
   }
 
   bool isFull()
@@ -49,6 +75,17 @@ public:
     storage.erase(getPositionAndValue(key).first);
     storage.emplace_front(key);
     searcher[key] = {value, storage.begin()};
+  }
+
+  void evict()
+  {
+    if (!storage.size())
+      return;
+    cout << storage.size() << " s " << endl;
+    cout << storage.back() << " back " << endl;
+    searcher.erase(storage.back());
+    storage.pop_back();
+    cout << storage.front() << " f" << endl;
   }
 
   V *get(K key)
@@ -68,21 +105,27 @@ public:
   void put(const K key, const V value)
   {
     if (isHere(key))
+    {
       update(key, value);
+      return;
+    }
     else
     {
       if (isFull())
-        deleteLeastVisited();
+        evict();
 
       storage.emplace_front(key);
       searcher[key] = {value, storage.begin()};
+      stats.keys++;
     }
   }
 
-  pair<K, V> mostRecentlyUsed()
+  pair<K, V> peak()
   {
     const K key = storage.front();
     const V value = searcher[key].first;
+    stats.hits++;
+
     return {key, value};
   }
 
@@ -90,23 +133,39 @@ public:
   {
     return {key, getPositionAndValue(key).second};
   }
+
+  void display()
+  {
+    for (const auto [k, v] : searcher)
+    {
+      cout << k << endl;
+    }
+  }
+
+  StatisticsDummy getStats()
+  {
+    return StatisticsDummy(stats.keys, stats.hits, stats.misses);
+  }
 };
 
 int main()
 {
-  LruCache<string, float> cache(3);
-
-  cache.put("hello", 89.888);
-  auto it = cache.get("hello");
-  if (it)
-    cout << *it << endl;
-
   LruCache<int, int> cache2(2);
   cache2.put(1, 2);
   cache2.put(3, 10);
-  int *ptr = cache2.get(3);
-  if (ptr)
-    cout << *ptr << endl;
+  cache2.get(1);
+  // if (ptr)
+  //   cout << *ptr << endl;
 
+  // cache2.display();
+
+  // cache2.evict();
+
+  // cache2.display();
+  // //   cache2.evict();
+
+  auto cache2Stats = cache2.getStats();
+  cout << cache2Stats.keys << ' ' << cache2Stats.hits << ' ' << cache2Stats.misses << endl;
+  cout << sizeof(cache2Stats) << endl;
   return 0;
 }
